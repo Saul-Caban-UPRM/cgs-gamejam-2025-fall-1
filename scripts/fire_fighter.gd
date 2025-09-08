f541e36c3c78486107a359bc1e41bcac711b5274
Adriana Isabel <adriana.carrasquillo1@upr.edu>
2025-09-07 22:32:25 -0400

    Added animations, platforms and background

--- scripts/fire_fighter.gd
+++ scripts/fire_fighter.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 @export var speed = 300       # horizontal speed
 @export var jump_velocity = -1000
 @export var gravity = 1200
+@onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	var direction = 0
@@ -25,5 +26,12 @@ func _physics_process(delta: float) -> void:
 		$Jump.play()
 		velocity.y = jump_velocity
 	
+	if direction != 0:
+		anim.play("walking_right")
+		anim.flip_h = direction < 0
+		
+	else:
+		anim.stop()                  
+		anim.frame = 0	
 	# Move the character
 	move_and_slide()
